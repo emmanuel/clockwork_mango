2b5f4ec54eb7861e318b57bd642df99e2da15eca
Emmanuel Gomez <emmanuel.gomez@trusera.com>
2008-09-26 11:56:47 -0700

    Added comments re: making DayPrecision aliasing conditional.

--- lib/clockwork/core_ext/day_precision.rb
+++ lib/clockwork/core_ext/day_precision.rb
@@ -3,9 +3,11 @@ module Clockwork
     module DayPrecision
       def self.included(base)
         base.module_eval do
-          alias_method :original_hour, :hour
-          alias_method :original_min,  :min
-          alias_method :original_sec,  :sec
+          # TODO: make this conditional so it doesn't blow up if it gets 
+          #   included in unanticipated places
+          alias_method :original_hour, :hour # if instance_methods.include?(:hour)
+          alias_method :original_min,  :min  # if instance_methods.include?(:min)
+          alias_method :original_sec,  :sec  # if instance_methods.include?(:sec)
           
           def hour
             nil
@@ -26,9 +28,11 @@ module Clockwork
     module DayPrecisionRemover
       def self.included(base)
         base.module_eval do
-          alias_method :hour, :original_hour
-          alias_method :min,  :original_min
-          alias_method :sec,  :original_sec
+          # TODO: make this conditional so it doesn't blow up if it gets 
+          #   included in unanticipated places
+          alias_method :hour, :original_hour # if instance_methods.include?(:original_hour)
+          alias_method :min,  :original_min  # if instance_methods.include?(:original_min)
+          alias_method :sec,  :original_sec  # if instance_methods.include?(:original_sec)
         end
       end
     end
