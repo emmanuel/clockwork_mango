d0241e13412c8205bfecc067da056c1041e7bdf6
Emmanuel Gomez <emmanuel.gomez@trusera.com>
2008-09-26 11:58:34 -0700

    Reorganized and RDoc'd Clockwork::Expression. Replaced NoMethodError raises with empty return values.

--- lib/clockwork/expression.rb
+++ lib/clockwork/expression.rb
@@ -1,21 +1,48 @@
 module Clockwork
   class Expression
-    def ===(other)
-      raise NoMethodError, "subclasses must define #==="
-    end
-    
+    # Disclose which attributes this Expression is concerned with
+    # 
+    # @return <Array[<Symbol>]> attribute names asserted in this expression
     def attributes
-      raise NoMethodError, "subclasses must define #attributes"
+      []
     end
     
+    # Test matchiness of "other" in "self". Subclasses should override this
+    # method to provide meaningful match semantics (eg., Assertion#===).
+    # 
+    # @param other<Object> an Object to be tested against this expression
+    # 
+    # @return <Boolean> does other<Object> match self?
+    def ===(other)
+      false
+    end
+    
+    # Intersect self with another Expression. The returned Intersection will
+    # only match if self and expression match.
+    # 
+    # @param expression<Clockwork::Expression>
+    # 
+    # @return <Clockwork::Intersection>
     def &(expression)
       Intersection.new(self, expression)
     end
     
+    # Union self with another Expression. The returned Union will
+    # match if either self or expression matches.
+    # 
+    # @param expression<Clockwork::Expression>
+    # 
+    # @return <Clockwork::Union>
     def |(expression)
       Union.new(self, expression)
     end
     
+    # Subtract an Expression from this one. The returned Difference will
+    # match if self matches and expression does not.
+    # 
+    # @param expression<Clockwork::Expression>
+    # 
+    # @return <Clockwork::Difference>
     def -(expression)
       Difference.new(self, expression)
     end
