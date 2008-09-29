119ba93831c001f366b27131fc0b234958ed163f
Emmanuel Gomez <emmanuel.gomez@trusera.com>
2008-09-28 23:10:36 -0700

    Redefined Clockwork::LIBDIR.

--- lib/clockwork.rb
+++ lib/clockwork.rb
@@ -10,15 +10,17 @@ module Clockwork
     self
   end # module VERSION
   
-  LIBDIR = Pathname(__FILE__).dirname.expand_path + "clockwork"
+  LIBDIR = Pathname(__FILE__).dirname.expand_path
 end # module Clockwork
 
-require Clockwork::LIBDIR + "expression"
-require Clockwork::LIBDIR + "assertion"
-require Clockwork::LIBDIR + "compound"
-require Clockwork::LIBDIR + "dsl"
-require Clockwork::LIBDIR + "core_ext"
+require Clockwork::LIBDIR + "clockwork/expression"
+require Clockwork::LIBDIR + "clockwork/assertion"
+require Clockwork::LIBDIR + "clockwork/compound"
+require Clockwork::LIBDIR + "clockwork/dsl"
+require Clockwork::LIBDIR + "clockwork/core_ext"
 
 def Clockwork
   yield(Clockwork::Dsl)
 end
+
+$LOAD_PATH << Clockwork::LIBDIR unless $LOAD_PATH.include?(Clockwork::LIBDIR)
--- lib/clockwork/core_ext.rb
+++ lib/clockwork/core_ext.rb
@@ -1,14 +1,14 @@
 require "date"
 require "time"
 
-require Clockwork::LIBDIR + "core_ext/day_precision"
+require Clockwork::LIBDIR + "clockwork/core_ext/day_precision"
 ::Date.send(:include, Clockwork::CoreExt::DayPrecision)
 ::DateTime.send(:include, Clockwork::CoreExt::DayPrecisionRemover)
 
-require Clockwork::LIBDIR + "core_ext/second_precision"
+require Clockwork::LIBDIR + "clockwork/core_ext/second_precision"
 ::DateTime.send(:include, Clockwork::CoreExt::SecondPrecision)
 
-require Clockwork::LIBDIR + "core_ext/human_date_values"
+require Clockwork::LIBDIR + "clockwork/core_ext/human_date_values"
 [::Date, ::Time, ::DateTime].each do |klass|
   klass.send(:include, Clockwork::CoreExt::HumanDateValues)
 end
