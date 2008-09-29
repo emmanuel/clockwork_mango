759dd9c231b59ef0f9ee502c8f333faa00790951
Emmanuel Gomez <emmanuel.gomez@trusera.com>
2008-09-28 22:08:32 -0700

    Reverted optional argument to HumanDateValues#yweek, #mweek, and #wday_in_month, split methods into previous + *_reverse.

--- lib/clockwork/core_ext/human_date_values.rb
+++ lib/clockwork/core_ext/human_date_values.rb
@@ -21,41 +21,45 @@ module Clockwork
         # self.class.days_in_year(self.year)
       end
       
-      # how many Sundays have occurred this year before today?
+      # how many Sundays have already occurred this year (before today)?
       # (similar to DateTime#cweek)
       # 
-      # @param reverse <Boolean> when true, calculates the reverse: 
-      #   is this the last week of the year? 2nd to last?
-      # 
-      # @return <Integer> count of Sundays before today in the current year, 
-      #   or negative count of Sundays remaining, if reverse is true
+      # @return <Integer> count of Sundays before today in the current year
       # 
       # TODO: spec
-      def yweek(reverse=false)
-        yday_of_preceding_sunday = self.yday - self.wday
-        if reverse
-          -((self.days_in_year - yday_of_preceding_sunday).div(7) + 1)
-        else
-          yday_of_preceding_sunday.div(7)
-        end
+      def yweek
+        (self.yday - self.wday).div(7)
+      end
+      
+      # how many Sundays are left this year (after today)?
+      # (similar to DateTime#cweek)
+      # 
+      # @return <Integer> negative count of Sundays remaining in the current year
+      # 
+      # TODO: spec
+      def yweek_reverse
+        yday_of_following_sunday = self.yday + (7 - self.wday)
+        -((self.days_in_year - yday_of_following_sunday).div(7) + 1)
       end
       
       # how many Sundays have occurred this month before today?
       # 
-      # @param reverse <Boolean> when true, calculates the reverse: 
-      #   is this the last week of the month? 2nd to last? 
+      # @return <Integer> count of Sundays before today in the current month
+      # 
+      # TODO: spec
+      def mweek
+        (self.mday - self.wday).div(7) + 1
+      end
+      
+      # how many Sundays have occurred this month before today?
       # 
       # @return <Integer> count of Sundays before today in the current month, 
       #   or negative count of Sundays remaining, if reverse is true
       # 
       # TODO: spec
-      def mweek(reverse=false)
-        mday_of_preceding_sunday = self.mday - self.wday
-        if reverse
-          -((self.days_in_month - mday_of_preceding_sunday).div(7) + 1)
-        else
-          mday_of_preceding_sunday.div(7) + 1
-        end
+      def mweek_reverse
+        mday_of_following_sunday = self.mday + (7 - self.wday)
+        -((self.days_in_month - mday_of_following_sunday).div(7) + 1)
       end
       
       # how many of this weekday have occurred this month? For example:
@@ -64,20 +68,26 @@ module Clockwork
       # 
       # considered naming this mwday_ordinal ... is that clearer?
       # 
-      # @param reverse <Boolean> when true, calculates the reverse: 
-      #   is this the last Monday of the month? 2nd to last Thursday?
-      # 
       # @return <Integer> count of occurrences of the current weekday in the 
-      #   current month before today, or negative count of remaining 
-      #   occurrences, if reverse is true
+      #   current month before today
       # 
       # TODO: spec
-      def wday_in_month(reverse=false)
-        if reverse
-          -((self.days_in_month - mday).div(7) + 1)
-        else
-          mday.div(7) + 1
-        end
+      def wday_in_month
+        mday.div(7) + 1
+      end
+      
+      # how many of this weekday have occurred this month? For example:
+      # If today is Thursday, which Thursday is this (the 1st, 2nd, etc.)?
+      # If Monday, which Monday (the 1st, 2nd, 3rd, 4th, etc.)?
+      # 
+      # considered naming this mwday_ordinal ... is that clearer?
+      # 
+      # @return <Integer> negative count of remaining occurrences of the 
+      #   current weekday in the current month
+      # 
+      # TODO: spec
+      def wday_in_month_reverse
+        -((self.days_in_month - mday).div(7) + 1)
       end
     end # module HumanDateValues
   end # module CoreExt
