b4c57960ac06108ed72662a5966a5c6c64b0722f
Emmanuel Gomez <emmanuel.gomez@trusera.com>
2008-10-01 00:51:16 -0700

    Added RDoc documentation for Clockwork method.

--- lib/clockwork.rb
+++ lib/clockwork.rb
@@ -20,6 +20,14 @@ require "clockwork/compound"
 require "clockwork/dsl"
 require "clockwork/core_ext"
 
-def Clockwork
+# Shortcut to the Clockwork::Dsl expression builder dsl. 
+# Lets you use the lib from any context like so:
+#   Clockwork { |c| c.november & c.monday & c.wday_in_month(1) }
+# which will return an expression that matches the 1st Monday in November
+# 
+# @param block <Block> block that will get Clockwork::Dsl as a parameter
+# 
+# @return <Clockwork::Expression> defined by the provided block
+def Clockwork(&block)
   yield(Clockwork::Dsl)
 end
