1188451686680f86691304e39b63a243af317cbb
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2009-02-18 00:36:59 -0800

    Touchups in Holidays.holiday? and HolidayMixin#has_holiday?.

--- lib/clockwork/holidays.rb
+++ lib/clockwork/holidays.rb
@@ -1,39 +1,32 @@
 module Clockwork
   module Holidays
-
-    def self.holiday?(date,region = nil)
-      regions = []
-      if region
-        regions = region.to_a
-      else
-        @@regions_cache ||= constants.map{|x| const_get(x) }
-        regions = @@regions_cache
-      end
-      regions.any?{|r| r.has_holiday?(date) }
+    def self.all_regions
+      @all_regions ||= constants.map { |x| const_get(x) }
     end
 
-    def self.include?(date)
-      holiday?(date)
+    def self.holiday?(date, region = nil)
+      search_regions = region ? Array(region) : all_regions
+      search_regions.any? { |r| r.has_holiday?(date) }
     end
+
+    alias_method :include?, :holiday?
+    alias_method :===,      :holiday?
   end
 
   module HolidayMixin
+    def all_holidays
+      @all_holidays ||= constants.map { |x| const_get(x) }
+    end
     # has_a_holiday?
     # accepts a date
     # creates an array of holidays for the module
     # checks to see if the date is in the array
     def has_holiday?(date)
-      all_holidays = constants.map{|x| const_get(x)}
-      all_holidays.any?{|h| h === date}
+      all_holidays.any? { |h| h === date }
     end
 
-    def include?(date)
-      has_holiday?(date)
-    end
-
-    def ===(date)
-      has_holiday?(date)
-    end
+    alias_method :include?, :has_holiday?
+    alias_method :===,      :has_holiday?
   end
 
 end
--- lib/clockwork/holidays/muslim.rb
+++ lib/clockwork/holidays/muslim.rb
@@ -3,10 +3,11 @@ require "clockwork" unless defined? Clockwork
 module Clockwork
   module Holidays
     module Muslim
-      FIRST_DAY_OF_RAMADAN = Clockwork {|c| } # September 2 (Islamic, moveable based on Lunar calendar)
-      EIDALFITRDAY_AFTER_THE_END_OF_RAMADAN = Clockwork {|c| } # October 2 (Islamic, moveable, based on lunar calendar)
-      extend HolidayMixin
 
+      FIRST_DAY_OF_RAMADAN = Clockwork {|c| } # September 2 (Islamic, moveable based on Lunar calendar)
+      EID_AL_FITR_DAY_AFTER_THE_END_OF_RAMADAN = Clockwork {|c| } # October 2 (Islamic, moveable, based on lunar calendar)
+
+      extend HolidayMixin
     end
   end
 end
