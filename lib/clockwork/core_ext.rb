e388a8681e84ce57235309faf17dc3766ac3c57b
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2009-02-23 00:17:21 -0800

    Made core_ext includes and extends more explicit.

--- lib/clockwork/core_ext.rb
+++ lib/clockwork/core_ext.rb
@@ -2,13 +2,18 @@ require "date"
 require "time"
 
 require "clockwork/core_ext/day_precision"
-::Date.send(:include, Clockwork::CoreExt::DayPrecision)
+::Date.send(    :include, Clockwork::CoreExt::DayPrecision)
 ::DateTime.send(:include, Clockwork::CoreExt::DayPrecisionRemover)
 
 require "clockwork/core_ext/second_precision"
 ::DateTime.send(:include, Clockwork::CoreExt::SecondPrecision)
 
 require "clockwork/core_ext/human_date_values"
-[::Date, ::Time, ::DateTime].each do |klass|
-  klass.send(:include, Clockwork::CoreExt::HumanDateValues)
-end
+::Date.send(    :include, Clockwork::CoreExt::HumanDateValues)
+::DateTime.send(:include, Clockwork::CoreExt::HumanDateValues)
+::Time.send(    :include, Clockwork::CoreExt::HumanDateValues)
+
+require "clockwork/core_ext/unit_values"
+::Date.send(    :extend, Clockwork::CoreExt::UnitValues::Date)
+::DateTime.send(:extend, Clockwork::CoreExt::UnitValues::Date)
+::Time.send(    :extend, Clockwork::CoreExt::UnitValues::Time)
