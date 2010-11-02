eab5826031b9061a50f735b3d594af5f6cf002f6
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2010-11-02 01:06:56 -0700

    Fix some small things and rename gemspec.

--- clockwork_mango.gemspec
+++ clockwork_mango.gemspec
@@ -4,13 +4,19 @@ require File.dirname(__FILE__) + '/lib/clockwork_mango/version'
 spec = Gem::Specification.new do |s|
   s.name = 'clockwork_mango'
   s.version = ClockworkMango.version
+
   s.author = "Emmanuel Gomez"
   s.email = "emmanuel.gomez@gmail.com"
   s.homepage = "http://github.com/emmanuel/clockwork_mango"
+
   s.summary = 'Temporal expression toolkit in Ruby.'
   s.description = <<-DESCRIPTION
     An(other) implementation of Temporal Expressions in Ruby.
     Describe recurrence in plain english!
   DESCRIPTION
+
+  s.files = Dir["lib/**/*.rb"]
+  s.files += Dir["spec/**/*.rb"]
+
   s.add_bundler_dependencies
 end
--- lib/clockwork_mango.rb
+++ lib/clockwork_mango.rb
@@ -1,8 +1,8 @@
 require "pathname"
 require "date"
 
-require "bundler"
-Bundler.setup(:default)
+# require "bundler"
+# Bundler.setup(:default)
 
 require "clockwork_mango/version"
 require "clockwork_mango/predicate"
@@ -12,12 +12,15 @@ require "clockwork_mango/compound_predicate"
 require "clockwork_mango/dsl"
 require "clockwork_mango/core_ext"
 
-# Shortcut to the ClockworkMango::Dsl expression builder dsl. 
+# Shortcut to the ClockworkMango::Dsl.predicate_builder dsl. 
 # Lets you use the lib from any context like so:
-#   ClockworkMango { |c| c.november & c.monday & c.wday_in_month(1) }
-# which will return an expression that matches the 1st Monday in November
+#   Clockwork { |c| c.november & c.wday_in_month(1) & c.monday }
+# or without the block variable:
+#   Clockwork { november & wday_in_month(1) & monday }
+# Both of which will return an expression that matches the 1st Monday in November
 # 
-# @param block <Block> block that will get ClockworkMango::Dsl as a parameter
+# @param block <Block> block that will get ClockworkMango::Dsl as a parameter,
+#   or instance_eval'd in the context of ClockworkMango::Dsl if arity zero
 # 
 # @return <ClockworkMango::Predicate> defined by the provided block
 def Clockwork(&block)
