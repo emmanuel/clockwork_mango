c0bcee6425f1211884a53d4d300fdbe8c7bbf136
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2010-11-05 02:09:40 -0700

    Update ClockworkMango::CoreExt::DayPrecision to use ActiveSupport::Concern.

--- lib/clockwork_mango/core_ext/day_precision.rb
+++ lib/clockwork_mango/core_ext/day_precision.rb
@@ -1,46 +1,48 @@
+require "active_support/concern"
+
 module ClockworkMango
   module CoreExt
     module DayPrecision
-      def self.included(base)
-        base.module_eval do
-          # TODO: make this conditional so it doesn't blow up if it gets 
-          #   included in unanticipated places
-          alias_method :hour_with_private, :hour # if instance_methods.include?(:hour)
-          alias_method :min_with_private,  :min  # if instance_methods.include?(:min)
-          alias_method :sec_with_private,  :sec  # if instance_methods.include?(:sec)
-          
-          def hour
-            nil
-          end
-          
-          alias_method :min,  :hour
-          alias_method :sec,  :hour
-          alias_method :usec, :hour
-          
-          public :hour
-          public :min
-          public :sec
-          public :usec
+      extend ActiveSupport::Concern
+
+      included do
+        # TODO: make this conditional so it doesn't blow up if it gets 
+        #   included in unanticipated places
+        alias_method :hour_with_private, :hour # if instance_methods.include?(:hour)
+        alias_method :min_with_private,  :min  # if instance_methods.include?(:min)
+        alias_method :sec_with_private,  :sec  # if instance_methods.include?(:sec)
+
+        def hour
+          nil
         end
+
+        alias_method :min,  :hour
+        alias_method :sec,  :hour
+        alias_method :usec, :hour
+
+        public :hour
+        public :min
+        public :sec
+        public :usec
       end
     end # module DayPrecision
-    
-    module DayPrecisionRemover
-      def self.included(base)
-        base.module_eval do
-          # TODO: make this conditional so it doesn't blow up if it gets 
-          #   included in unanticipated places
-          alias_method :hour, :hour_with_private # if instance_methods.include?(:original_hour)
-          alias_method :min,  :min_with_private  # if instance_methods.include?(:original_min)
-          alias_method :sec,  :sec_with_private  # if instance_methods.include?(:original_sec)
 
-          public :hour
-          public :min
-          public :sec
-          public :usec
-        end
+    module DayPrecisionRemover
+      extend ActiveSupport::Concern
+
+      included do
+        # TODO: make this conditional so it doesn't blow up if it gets 
+        #   included in unanticipated places
+        alias_method :hour, :hour_with_private # if instance_methods.include?(:original_hour)
+        alias_method :min,  :min_with_private  # if instance_methods.include?(:original_min)
+        alias_method :sec,  :sec_with_private  # if instance_methods.include?(:original_sec)
+
+        public :hour
+        public :min
+        public :sec
+        public :usec
       end
-    end
+    end # module DayPrecisionRemover
   end # module CoreExt
 end # module ClockworkMango
 
