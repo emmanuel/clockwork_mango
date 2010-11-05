44b10d698fc5b0db279749488ae7e5f3b3ead9d8
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2010-11-05 02:00:11 -0700

    Update CompoundPredicateSpec to use RSpec 2 shortcuts.

--- spec/clockwork_mango/compound_predicate_spec.rb
+++ spec/clockwork_mango/compound_predicate_spec.rb
@@ -1,106 +1,100 @@
 require "spec_helper"
 
 describe ClockworkMango::CompoundPredicate do
-  before :all do
-    @time     = Time.local(*TIME_ATTRIBUTES.map { |a| VALUES[a] })
-    @datetime = DateTime.new(*DATETIME_ATTRS.map { |a| VALUES[a] })
-    @date     = Date.new(*DATE_ATTRIBUTES.map { |a| VALUES[a] })
+  let(:time)     { Time.local(*TIME_ATTRIBUTES.map { |a| VALUES[a] }) }
+  let(:datetime) { DateTime.new(*DATETIME_ATTRS.map { |a| VALUES[a] }) }
+  let(:date)     { Date.new(*DATE_ATTRIBUTES.map { |a| VALUES[a] }) }
 
-    @year  = ClockworkMango::ComparisonPredicate.new(:year, 2008)
-    @month = ClockworkMango::ComparisonPredicate.new(:month, 9)
-    @mday  = ClockworkMango::ComparisonPredicate.new(:mday, 24)
+  let(:year_08) { ClockworkMango::ComparisonPredicate.new(:year, 2008) }
+  let(:month)   { ClockworkMango::ComparisonPredicate.new(:month, 9) }
+  let(:mday)    { ClockworkMango::ComparisonPredicate.new(:mday, 24) }
 
-    @hour = ClockworkMango::ComparisonPredicate.new(:hour, 18)
-    @min  = ClockworkMango::ComparisonPredicate.new(:min, 30)
-    @sec  = ClockworkMango::ComparisonPredicate.new(:sec, 15)
+  let(:hour) { ClockworkMango::ComparisonPredicate.new(:hour, 18) }
+  let(:min)  { ClockworkMango::ComparisonPredicate.new(:min, 30) }
+  let(:sec)  { ClockworkMango::ComparisonPredicate.new(:sec, 15) }
 
-    @yday  = ClockworkMango::ComparisonPredicate.new(:yday, 268)
-    @yweek = ClockworkMango::ComparisonPredicate.new(:mday, 0)
-    @wday  = ClockworkMango::ComparisonPredicate.new(:wday, 3)
-  end
+  let(:yday)  { ClockworkMango::ComparisonPredicate.new(:yday, 268) }
+  let(:yweek) { ClockworkMango::ComparisonPredicate.new(:mday, 0) }
+  let(:wday)  { ClockworkMango::ComparisonPredicate.new(:wday, 3) }
 
   describe "IntersectionPredicate" do
-    before :all do
-      @sep_24 = ClockworkMango::IntersectionPredicate.new(@year, @mday)
+    subject { ClockworkMango::IntersectionPredicate.new(year_08, mday) }
+
+    it "should match Times when both expressions match" do
+      should === time
     end
 
-    it "should match Time objects when both expressions match" do
-      @sep_24.should === @time
+    it "should match DateTimes when both expressions match" do
+      should === datetime
     end
 
-    it "should match DateTime objects when both expressions match" do
-      @sep_24.should === @datetime
+    it "should match Dates when both expressions match" do
+      should === date
     end
 
-    it "should match Date objects when both expressions match" do
-      @sep_24.should === @date
+    it "should not match Times when one expression does not match" do
+      should_not === (time + (1 * 24 * 60 * 60))
     end
 
-    it "should not match Date objects when one expression does not match" do
-      @sep_24.should_not === (@time + (1 * 24 * 60 * 60))
+    it "should not match Dates when one expression does not match" do
+      should_not === (datetime + 1)
     end
 
-    it "should not match Date objects when one expression does not match" do
-      @sep_24.should_not === (@datetime + 1)
-    end
-
-    it "should not match Date objects when one expression does not match" do
-      @sep_24.should_not === (@date + 1)
+    it "should not match Dates when one expression does not match" do
+      should_not === (date + 1)
     end
   end
 
   describe "UnionPredicate" do
-    before :all do
-      @year_2006 = ClockworkMango::ComparisonPredicate.new(:year, 2006)
-      @union = ClockworkMango::UnionPredicate.new(@year, @year_2006)
-      @time_in_06 = DateTime.civil(2006, 6, 1, 12, 0, 0)
+    let(:year_06) { ClockworkMango::ComparisonPredicate.new(:year, 2006) }
+    let(:time_in_06) { DateTime.civil(2006, 6, 1, 12, 0, 0) }
+
+    subject { ClockworkMango::UnionPredicate.new(year_08, year_06) }
+
+    it "should match Times when either expression matches" do
+      should === time
     end
 
-    it "should match when either expression matches" do
-      @union.should === @time
+    it "should match DateTimes when either expression matches" do
+      should === datetime
     end
 
-    it "should match when either expression matches" do
-      @union.should === @datetime
+    it "should match Dates when either expression matches" do
+      should === date
     end
 
-    it "should match when either expression matches" do
-      @union.should === @date
+    it "should match DateTimes when either expression matches" do
+      should === time_in_06
     end
 
-    it "should match when either expression matches" do
-      @union.should === @time_in_06
+    it "should not match Times when neither expression matches" do
+      should_not === (time + (365 * 24 * 60 * 60))
     end
 
-    it "should not match when neither expression matches" do
-      @union.should_not === (@time + (365 * 24 * 60 * 60))
-    end
-
-    it "should not match when neither expression matches" do
-      @union.should_not === (@time_in_06 - (365 * 24 * 60 * 60))
+    it "should not match DateTimes when neither expression matches" do
+      should_not === (time_in_06 - (365 * 24 * 60 * 60))
     end
   end
 
   describe "DifferencePredicate" do
-    before :all do
-      @year_2007 = ClockworkMango::ComparisonPredicate.new(:year, 2007)
-      @difference = ClockworkMango::DifferencePredicate.new(@year, @year_2007)
+    let(:year_07) { ClockworkMango::ComparisonPredicate.new(:year, 2007) }
+
+    subject { ClockworkMango::DifferencePredicate.new(year_08, year_07) }
+
+    it "should match Times when the 1st expression matches and the 2nd doesn't" do
+      should === time
     end
 
-    it "should match when the 1st expression matches and the 2nd doesn't" do
-      @difference.should === @time
+    it "should match DateTimes when the 1st expression matches and the 2nd doesn't" do
+      should === datetime
     end
 
-    it "should match when the 1st expression matches and the 2nd doesn't" do
-      @difference.should === @datetime
+    it "should match Dates when the 1st expression matches and the 2nd doesn't" do
+      should === date
     end
 
-    it "should match when the 1st expression matches and the 2nd doesn't" do
-      @difference.should === @date
-    end
-
-    it "should not match when the 1st expression matches and the 2nd does, too" do
-      @difference.should_not === (@date - 365)
+    it "should not match Dates when the 1st expression matches and the 2nd does, too" do
+      should_not === (date - 365)
     end
   end
 
