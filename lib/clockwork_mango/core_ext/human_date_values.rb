ab79fd9e2b9d27e1aa4696cccf0df39823c54deb
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2010-11-09 00:33:47 -0800

    Add spec coverage for CoreExt::HumanDateValues methods.

--- lib/clockwork_mango/core_ext/human_date_values.rb
+++ lib/clockwork_mango/core_ext/human_date_values.rb
@@ -33,67 +33,84 @@ module ClockworkMango
       # 
       # @return [Integer] count of Sundays before today in the current year
       def yweek
-        -(self.wday - self.yday).div(7)
+        (self.yday + self.beginning_of_year.wday - 2).div(7)
       end
 
       # how many Sundays are left this year (after today)?
       # (similar to DateTime#cweek)
       # 
-      # @return [Integer] negative count of Sundays remaining in the current year
+      #   DateTime.parse("2011-01-01").yweek_reverse # => 52
+      #   DateTime.parse("2010-11-08").yweek_reverse # => 7
+      #   DateTime.parse("2010-12-08").yweek_reverse # => 3
       # 
-      # TODO: spec
+      # @return [Integer] count of Sundays remaining in the current year
       def yweek_reverse
+        last_day_of_year = end_of_year
+        yday_of_last_sunday_of_year = last_day_of_year.yday - last_day_of_year.wday
         yday_of_following_sunday = self.yday + (7 - self.wday)
-        -((self.days_in_year - yday_of_following_sunday).div(7) + 1)
+        (yday_of_last_sunday_of_year - yday_of_following_sunday).div(7) + 1
       end
-      
+
       # how many Sundays have occurred this month before today?
       # 
-      # @return [Integer] count of Sundays before today in the current month
+      #   DateTime.parse("2011-01-01").mweek # => 0
+      #   DateTime.parse("2011-01-03").mweek # => 1
+      #   DateTime.parse("2004-02-29").mweek # => 4
+      #   DateTime.parse("2010-11-08").mweek # => 1
       # 
-      # TODO: spec
+      # @return [Integer] count of Sundays before today in the current month
       def mweek
-        (self.mday - self.wday).div(7) + 1
+        (self.mday + self.beginning_of_month.wday - 1).div(7)
       end
-      
+
       # how many Sundays are left this month after today?
       # 
-      # @return [Integer] negative count of Sundays remaining
+      #   DateTime.parse("2011-01-01").mweek_reverse # => 5
+      #   DateTime.parse("2011-01-03").mweek_reverse # => 4
+      #   DateTime.parse("2004-02-29").mweek_reverse # => 0
+      #   DateTime.parse("2010-11-08").mweek_reverse # => 3
       # 
-      # TODO: spec
+      # @return [Integer] negative count of Sundays remaining
       def mweek_reverse
         mday_of_following_sunday = self.mday + (7 - self.wday)
-        -((self.days_in_month - mday_of_following_sunday).div(7) + 1)
+        ((self.days_in_month - mday_of_following_sunday).div(7) + 1)
       end
-      
+
       # how many of this weekday have occurred this month? For example:
       # If today is Thursday, which Thursday is this (the 1st, 2nd, etc.)?
       # If Monday, which Monday (the 1st, 2nd, 3rd, 4th, etc.)?
       # 
       # considered naming this mwday_ordinal ... is that clearer?
       # 
+      #   DateTime.parse("2011-01-01").wday_in_month # => 1
+      #   DateTime.parse("2011-01-03").wday_in_month # => 1
+      #   DateTime.parse("2004-02-29").wday_in_month # => 5
+      #   DateTime.parse("2010-11-08").wday_in_month # => 2
+      # 
       # @return [Integer]
       #   count of occurrences of the current weekday in the
       #   current month before today
-      # 
-      # TODO: spec
       def wday_in_month
         self.mday.div(7) + 1
       end
-      
+
       # how many occurrences of this weekday are left this month?
       # For example:
-      #   If today is Thursday, which Thursday is this (the 1st, 2nd, etc.)?
-      #   If Monday, which Monday (the 1st, 2nd, 3rd, 4th, etc.)?
+      #   If today is Thursday, how many more Thursdays are in this month (1, 2, etc.)?
+      #   If Monday, how many Mondays remain (1, 2, 3, 4, etc.)?
       # 
-      # considered naming this "mwday_ordinal" ... is that clearer?
+      # considered naming this "mwday_ordinal_reverse" ... is that clearer?
+      # 
+      #   DateTime.parse("2011-01-01").wday_in_month_reverse # => 4
+      #   DateTime.parse("2011-01-03").wday_in_month_reverse # => 4
+      #   DateTime.parse("2004-02-29").wday_in_month_reverse # => 0
+      #   DateTime.parse("2010-11-08").wday_in_month_reverse # => 3
+      #   DateTime.parse("2010-11-18").wday_in_month_reverse # => 1
       # 
       # @return [Integer] negative count of remaining occurrences of the 
       #   current weekday in the current month
-      # 
-      # TODO: spec
       def wday_in_month_reverse
-        -((self.days_in_month - self.mday).div(7) + 1)
+        (self.days_in_month - self.mday).div(7)
       end
     end # module HumanDateValues
   end # module CoreExt
--- spec/clockwork_mango/core_ext/human_date_values_spec.rb
+++ spec/clockwork_mango/core_ext/human_date_values_spec.rb
@@ -46,10 +46,106 @@ module ClockworkMango
       end
 
       it "should return 44 when called on 2010-11-08" do
-        DateTime.parse("2010-11-08").yweek.should == 44
+        DateTime.parse("2010-11-08").yweek.should == 45
       end
-    end
+    end # describe "#yweek"
 
-    end
+    describe "#yweek_reverse" do
+      it "should return 52 when called on 2011-01-01" do
+        DateTime.parse("2011-01-01").yweek_reverse.should == 52
+      end
+
+      it "should return 7 when called on 2010-11-08" do
+        DateTime.parse("2010-11-08").yweek_reverse.should == 7
+      end
+
+      it "should return 3 when called on 2010-12-08" do
+        DateTime.parse("2010-12-08").yweek_reverse.should == 3
+      end
+    end # describe "#yweek_reverse"
+
+    describe "#mweek" do
+      it "should return 0 when called before the first Sunday of the month" do
+        DateTime.parse("2011-01-01").mweek.should == 0
+      end
+
+      it "should return 1 when called after the first Sunday of the month, before the second" do
+        DateTime.parse("2011-01-03").mweek.should == 1
+      end
+
+      it "should return 4 when called on 2004-02-29" do
+        DateTime.parse("2004-02-29").mweek.should == 4
+      end
+
+      it "should return 1 when called on 2010-11-08" do
+        DateTime.parse("2010-11-08").mweek.should == 1
+      end
+
+      it "should return 2 when called on 2010-11-18" do
+        DateTime.parse("2010-11-18").mweek.should == 2
+      end
+    end # describe "#mweek"
+
+    describe "#mweek_reverse" do
+      it "should return 5 when called on 2011-01-01" do
+        DateTime.parse("2011-01-01").mweek_reverse.should == 5
+      end
+
+      it "should return 4 when called on 2011-01-03" do
+        DateTime.parse("2011-01-03").mweek_reverse.should == 4
+      end
+
+      it "should return 0 when called on 2004-02-29" do
+        DateTime.parse("2004-02-29").mweek_reverse.should == 0
+      end
+
+      it "should return 3 when called on 2010-11-08" do
+        DateTime.parse("2010-11-08").mweek_reverse.should == 3
+      end
+
+      it "should return 2 when called on 2010-11-18" do
+        DateTime.parse("2010-11-18").mweek_reverse.should == 2
+      end
+    end # describe "#mweek_reverse"
+
+    describe "#wday_in_month" do
+      it "should return 1 when called on 2011-01-01" do
+        DateTime.parse("2011-01-01").wday_in_month.should == 1
+      end
+
+      it "should return 1 when called on 2011-01-03" do
+        DateTime.parse("2011-01-03").wday_in_month.should == 1
+      end
+
+      it "should return 5 when called on 2004-02-29" do
+        DateTime.parse("2004-02-29").wday_in_month.should == 5
+      end
+
+      it "should return 2 when called on 2010-11-08" do
+        DateTime.parse("2010-11-08").wday_in_month.should == 2
+      end
+    end # describe "#wday_in_month"
+
+    describe "#wday_in_month_reverse" do
+      it "should return 4 when called on 2011-01-01" do
+        DateTime.parse("2011-01-01").wday_in_month_reverse.should == 4
+      end
+
+      it "should return 4 when called on 2011-01-03" do
+        DateTime.parse("2011-01-03").wday_in_month_reverse.should == 4
+      end
+
+      it "should return 0 when called on 2004-02-29" do
+        DateTime.parse("2004-02-29").wday_in_month_reverse.should == 0
+      end
+
+      it "should return 3 when called on 2010-11-08" do
+        DateTime.parse("2010-11-08").wday_in_month_reverse.should == 3
+      end
+
+      it "should return 1 when called on 2010-11-18" do
+        DateTime.parse("2010-11-18").wday_in_month_reverse.should == 1
+      end
+    end # describe "#wday_in_month_reverse"
   end # describe CoreExt::HumanDateValues
 end # module ClockworkMango
