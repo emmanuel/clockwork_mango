3efccd8846bfdd515b4704e9b567bbec4beed9d3
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2010-11-08 20:22:09 -0800

    Move Predicate spec into ClockworkMango module.

--- spec/clockwork_mango/predicate_spec.rb
+++ spec/clockwork_mango/predicate_spec.rb
@@ -1,32 +1,32 @@
 require "spec_helper"
 
-describe ClockworkMango::Predicate do
-  before :all do
-    @time     = Time.local(*TIME_ATTRIBUTES.map { |a| VALUES[a] })
-    @datetime = DateTime.new(*DATETIME_ATTRS.map { |a| VALUES[a] })
-    @date     = Date.new(*DATE_ATTRIBUTES.map { |a| VALUES[a] })
-
-    @year  = ClockworkMango::ComparisonPredicate.new(:year, 2008)
-    @month = ClockworkMango::ComparisonPredicate.new(:month, 9)
-    @mday  = ClockworkMango::ComparisonPredicate.new(:mday, 24)
-
-    @hour = ClockworkMango::ComparisonPredicate.new(:hour, 18)
-    @min  = ClockworkMango::ComparisonPredicate.new(:min, 30)
-    @sec  = ClockworkMango::ComparisonPredicate.new(:sec, 15)
-
-    @yday  = ClockworkMango::ComparisonPredicate.new(:yday, 268)
-    @yweek = ClockworkMango::ComparisonPredicate.new(:mday, 0)
-    @wday  = ClockworkMango::ComparisonPredicate.new(:wday, 3)
-  end
-
-  describe "Predicate" do
+module ClockworkMango
+  describe Predicate do
     before :all do
-      @sep_24 = ClockworkMango::IntersectionPredicate.new(@year, @mday)
+      @time     = Time.local(*TIME_ATTRIBUTES.map { |a| VALUES[a] })
+      @datetime = DateTime.new(*DATETIME_ATTRS.map { |a| VALUES[a] })
+      @date     = Date.new(*DATE_ATTRIBUTES.map { |a| VALUES[a] })
+
+      @year  = ComparisonPredicate.new(:year, 2008)
+      @month = ComparisonPredicate.new(:month, 9)
+      @mday  = ComparisonPredicate.new(:mday, 24)
+
+      @hour = ComparisonPredicate.new(:hour, 18)
+      @min  = ComparisonPredicate.new(:min, 30)
+      @sec  = ComparisonPredicate.new(:sec, 15)
+
+      @yday  = ComparisonPredicate.new(:yday, 268)
+      @yweek = ComparisonPredicate.new(:mday, 0)
+      @wday  = ComparisonPredicate.new(:wday, 3)
     end
 
-    it "should report the attribute(s) it asserts wrong" do
-      
-    end
-  end
+    describe "Predicate" do
+      before :all do
+        @sep_24 = IntersectionPredicate.new(@year, @mday)
+      end
 
-end
+      it "should report the attribute(s) it asserts wrong"
+    end
+
+  end # describe Predicate
+end # module ClockworkMango
