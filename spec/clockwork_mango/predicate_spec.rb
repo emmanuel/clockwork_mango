1dc7c2481fb7becaaca223d28e9e739959b3ed4c
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2010-11-11 00:15:06 -0800

    Spec OffsetPredicate a little more and clean Predicate spec.

--- spec/clockwork_mango/predicate_spec.rb
+++ spec/clockwork_mango/predicate_spec.rb
@@ -2,31 +2,54 @@ require "spec_helper"
 
 module ClockworkMango
   describe Predicate do
-    before :all do
-      @time     = Time.local(*TIME_ATTRIBUTES.map { |a| VALUES[a] })
-      @datetime = DateTime.new(*DATETIME_ATTRS.map { |a| VALUES[a] })
-      @date     = Date.new(*DATE_ATTRIBUTES.map { |a| VALUES[a] })
+    let(:time)     { Time.local(*TIME_ATTRIBUTES.map { |a| VALUES[a] }) }
+    let(:datetime) { DateTime.new(*DATETIME_ATTRS.map { |a| VALUES[a] }) }
+    let(:date)     { Date.new(*DATE_ATTRIBUTES.map { |a| VALUES[a] }) }
 
-      @year  = ComparisonPredicate.new(:year, 2008)
-      @month = ComparisonPredicate.new(:month, 9)
-      @day  = ComparisonPredicate.new(:day, 24)
+    let(:year)  { EqualityPredicate.new(:year, 2008) }
+    let(:month) { EqualityPredicate.new(:month, 9) }
+    let(:day)   { EqualityPredicate.new(:day, 24) }
 
-      @hour = ComparisonPredicate.new(:hour, 18)
-      @min  = ComparisonPredicate.new(:min, 30)
-      @sec  = ComparisonPredicate.new(:sec, 15)
+    let(:hour) { EqualityPredicate.new(:hour, 18) }
+    let(:min)  { EqualityPredicate.new(:min, 30) }
+    let(:sec)  { EqualityPredicate.new(:sec, 15) }
 
-      @yday  = ComparisonPredicate.new(:yday, 268)
-      @yweek = ComparisonPredicate.new(:day, 0)
-      @wday  = ComparisonPredicate.new(:wday, 3)
-    end
+    let(:yday)  { EqualityPredicate.new(:yday, 268) }
+    let(:yweek) { EqualityPredicate.new(:day, 0) }
+    let(:wday)  { EqualityPredicate.new(:wday, 3) }
 
-    describe "Predicate" do
-      before :all do
-        @sep_24 = IntersectionPredicate.new(@year, @day)
+    describe "#inspect" do
+      it "should return itself as #to_temporal_expression" do
+        year.inspect.should ==
+          "<ClockworkMango::EqualityPredicate:#{year.object_id.to_s(16)} temporal_expression=#{year.to_temporal_expression.inspect}>"
       end
+    end # describe "#inspect"
 
-      it "should report the attribute(s) it asserts wrong"
-    end
+    describe "#|" do
+      
+    end # describe "#|"
+
+    describe "#&" do
+      
+    end # describe "#&"
+
+    describe "#-" do
+      
+    end # describe "#-"
+
+    describe "#>>" do
+      context "when called with a two element array" do
+        subject { year >> [:years, 1] }
+
+        it "should return an OffsetPredicate" do
+          should be_kind_of(OffsetPredicate)
+        end
+
+        it "should be offsetted from the receiver" do
+          should == OffsetPredicate.new(year, :years, 1)
+        end
+      end # context "when called with a two element array"
+    end # describe "#>>"
 
   end # describe Predicate
 end # module ClockworkMango
