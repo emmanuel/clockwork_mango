1b8f672eec41ce9af005d24a72a08af28532f8e5
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2011-03-30 16:43:54 -0700

    Break up some examples in HumanDateValues spec.

--- spec/clockwork_mango/core_ext/human_date_values_spec.rb
+++ spec/clockwork_mango/core_ext/human_date_values_spec.rb
@@ -7,31 +7,51 @@ require "clockwork_mango/core_ext/human_date_values"
 module ClockworkMango
   describe CoreExt::HumanDateValues do
     describe "#leap_year?, #days_in_month, #days_in_year" do
-      let(:leap_year_4yr)      { DateTime.new(2004, 2, 29) } # => Sun Feb 29 00:00:00 -0000 2004
-      let(:no_leap_year_100yr) { DateTime.new(2100, 2, 28) } # => Sun Feb 28 00:00:00 -0000 2100
-      let(:leap_year_400yr)    { DateTime.new(2000, 2, 29) } # => Tue Feb 29 00:00:00 -0000 2000
-
       context "on a 4yr leap year" do
-        it "should correctly report the number of days in Feb of a 4yr leap year" do
-          leap_year_4yr.leap_year?.should == true
-          leap_year_4yr.days_in_month.should == 29
-          leap_year_4yr.days_in_year.should == 366
+        subject { DateTime.new(2004, 2, 29) } # => Sun Feb 29 00:00:00 -0000 2004
+
+        it "returns true for #leap_year?" do
+          subject.should be_leap_year
+        end
+
+        it "reports the correct number of days in Feb" do
+          subject.days_in_month.should == 29
+        end
+
+        it "reports the correct number of days in the year" do
+          subject.days_in_year.should == 366
         end
       end
 
       context "on a 100yr leap year" do
-        it "should correctly report the number of days in Feb of a 100yr leap year" do
-          no_leap_year_100yr.leap_year?.should == false
-          no_leap_year_100yr.days_in_month.should == 28
-          no_leap_year_100yr.days_in_year.should == 365
+        subject { DateTime.new(2100, 2, 28) } # => Sun Feb 28 00:00:00 -0000 2100
+
+        it "returns true for #leap_year?" do
+          subject.should_not be_leap_year
+        end
+
+        it "reports the correct number of days in Feb" do
+          subject.days_in_month.should == 28
+        end
+
+        it "reports the correct number of days in the year" do
+          subject.days_in_year.should == 365
         end
       end
 
       context "on a 400yr leap year" do
-        it "should correctly report the number of days in Feb" do
-          leap_year_400yr.leap_year?.should == true
-          leap_year_400yr.days_in_month.should == 29
-          leap_year_400yr.days_in_year.should == 366
+        subject { DateTime.new(2000, 2, 29) } # => Tue Feb 29 00:00:00 -0000 2000
+
+        it "returns true for #leap_year?" do
+          subject.should be_leap_year
+        end
+
+        it "reports the correct number of days in Feb" do
+          subject.days_in_month.should == 29
+        end
+
+        it "reports the correct number of days in the year" do
+          subject.days_in_year.should == 366
         end
       end
     end # describe "#leap_year?, #days_in_month, #days_in_year"
