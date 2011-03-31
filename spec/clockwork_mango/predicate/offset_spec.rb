442681c82bd3af5a84e713361edbdc4bb9889ce7
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2011-03-31 14:53:36 -0700

    Tighten Predicate::Offset spec.

--- spec/clockwork_mango/predicate/offset_spec.rb
+++ spec/clockwork_mango/predicate/offset_spec.rb
@@ -9,41 +9,43 @@ module ClockworkMango
         Predicate::Equality.new(:wday, 4) &         # Thursday
         Predicate::Equality.new(:wday_in_month, 4)  # Fourth Thursday of the month
     }
+    # 2010-11-25 is the 4th Thursday of November in 2010
+    let(:matching_date) { DateTime.parse("2010-11-25") }
+    let(:unit) { :days }
+    let(:value) { 1 }
+
+    subject { Predicate::Offset.new(predicate, unit, value) }
+
+    it "should equal itself initialized with a singular" do
+      singular_unit = unit.to_s.chomp("s").to_sym
+      subject.should == Predicate::Offset.new(predicate, singular_unit, value)
+    end
 
     context "when initialized with a positive value" do
-      subject { Predicate::Offset.new(predicate, :days, 1) }
+      let(:value) { 1 }
+      let(:offset_date) { matching_date + value }
 
       it { should be_kind_of(Predicate::Offset) }
-      it { subject.should express([:>>, predicate.to_temporal_sexp, :days, 1]) }
-      it "should equal itself initialized with a singular" do
-        should == Predicate::Offset.new(predicate, :day, 1)
-      end
-      # 2010-11-25 is the 4th Thursday
-      it { should === DateTime.parse("2010-11-26") }
+      it { should express([:>>, predicate.to_temporal_sexp, unit, value]) }
+      it { should === offset_date }
     end
 
     context "when initialized with a negative value" do
-      subject { Predicate::Offset.new(predicate, :days, -1) }
+      let(:value) { -1 }
+      let(:offset_date) { matching_date + value }
 
       it { should be_kind_of(Predicate::Offset) }
-      it { subject.should express([:>>, predicate.to_temporal_sexp, :days, -1]) }
-      it "should equal itself initialized with a singular" do
-        should == Predicate::Offset.new(predicate, :day, -1)
-      end
-      # 2010-11-25 is the 4th Thursday
-      it { should === DateTime.parse("2010-11-24") }
+      it { should express([:>>, predicate.to_temporal_sexp, unit, value]) }
+      it { should === offset_date }
     end
 
     context "when initialized with a zero value" do
-      subject { Predicate::Offset.new(predicate, :days, 0) }
+      let(:value) { 0 }
+      let(:offset_date) { matching_date + value }
 
       it { should be_kind_of(Predicate::Offset) }
-      it { subject.should express([:>>, predicate.to_temporal_sexp, :days, 0]) }
-      it "should equal itself initialized with a singular" do
-        should == Predicate::Offset.new(predicate, :day, 0)
-      end
-      # 2010-11-25 was the 4th Thursday in November
-      it { should === DateTime.parse("2010-11-25") }
+      it { should express([:>>, predicate.to_temporal_sexp, unit, value]) }
+      it { should === offset_date }
     end
   end
 end
